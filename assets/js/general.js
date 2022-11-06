cf3e380297fae8ba87b098acc0089ee48fcf930a
Fran López <fran2002mc@gmail.com>
2022-11-06 16:20:31 +0100

    .

--- assets/js/general.js
+++ assets/js/general.js
@@ -30,23 +30,22 @@ const createHeader = () => {
     a1.textContent = 'Inicio';
     const li2 = document.createElement('li');
     const a2 = document.createElement('a');
-    a2.setAttribute('href', 'onfire.dixho.com/about.html');
+    a2.setAttribute('href', './about.html');
     a2.textContent = 'Acerca de';
-    const li3 = document.createElement('li');
-    const a3 = document.createElement('a');
-    a3.setAttribute('href', 'onfire.dixho.com/contact.html');
-    a3.textContent = 'Contacto';
+
 
     li1.appendChild(a1);
     li2.appendChild(a2);
-    li3.appendChild(a3);
+
     ul.appendChild(li1);
     ul.appendChild(li2);
-    ul.appendChild(li3);
+ 
     nav.appendChild(ul);
     header.appendChild(nav);
-    document.body.append(header);
-    document.body.appendChild(document.createElement('hr'));
+
+    document.body.insertBefore(document.createElement('hr'), document.body.firstChild);
+    document.body.insertBefore(header, document.body.firstChild);
+    
 
 
 
